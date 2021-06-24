800bbb6443f0112db15306a088b79010267d1681
Rubin Raithel <33808743+Coronon@users.noreply.github.com>
2021-06-25 00:36:33 +0200

    (Lib + Networking) remove redundant MiddlewareRunner

--- lib/src/networking/websockets/middleware.dart
+++ lib/src/networking/websockets/middleware.dart
@@ -8,31 +8,3 @@ enum MiddlewareAction {
 ///
 /// Arguments: Calling [Session] instance, message, [MiddlewareAction] action and a proxy to the next [MiddlewareFunc].
 typedef MiddlewareFunc = void Function(dynamic, dynamic, MiddlewareAction, void Function(dynamic));
-
-class MiddlewareRunner<T> {
-  /// List of installed middleware.
-  ///
-  /// Each middleware recieves the calling [Session] instance, message,
-  /// [MiddlewareAction] and a proxy to the next [MiddlewareFunc].
-  List<MiddlewareFunc> middleware = <MiddlewareFunc>[];
-
-  late T ref;
-
-  late int _i;
-
-  late MiddlewareAction _action;
-
-  /// Run all installed middleware on given message and action
-  dynamic run(dynamic message, MiddlewareAction action) {
-    _i = 0;
-    _action = action;
-    return _next(message);
-  }
-
-  dynamic _next(dynamic message) {
-    if (_i == middleware.length) return message;
-    _i += 1;
-
-    middleware[_i - 1](ref, message, _action, _next);
-  }
-}
