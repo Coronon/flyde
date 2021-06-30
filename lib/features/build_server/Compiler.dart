ee8c0e4efdd348657dbf417ea1cff8f53cbf6697
Henrik <henrik@thoroee.com>
2021-06-30 22:38:03 +0200

    Add first unfinished version of Compiler API

---
+++ lib/features/build_server/Compiler.dart
@@ -0,0 +1,29 @@
+class Compiler {
+  /// Settings for compilation.
+  /// NOTE: Type must be changed to explicit
+  dynamic _config;
+
+  /// Map of all project files.
+  /// Key: Unique file ID.
+  /// Value: File hash code.
+  Map<String, String> _files = {};
+
+  /// Internal file cache. Required for storing source, object and executable files.
+  /// NOTE: Type must be changed to explicit
+  dynamic _cache;
+
+  Compiler(this._config, this._files, this._cache);
+
+  /// Returns a `List` of all file ids which are out of date and need to be synced using `addFile`.
+  List<String> get outdatedFiles => [];
+
+  /// `true` if any of the source files changed since last compilation.
+  bool get requiresRecompile => true;
+
+  /// Adds a file to internal cache.
+  /// Required for each source file.
+  void addFile(dynamic file) async {}
+
+  /// Compiles
+  void compile() async {}
+}
