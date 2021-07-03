3c9ee45ba2b64aa901d67c12cf4c436b24685909
Henrik <henrik@thoroee.com>
2021-07-03 14:18:10 +0200

    Redefine compiler configuration

--- lib/core/fs/configs/compiler_config.dart
+++ lib/core/fs/configs/compiler_config.dart
@@ -1,60 +1,52 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'compiler_config.g.dart';
+
+enum CompilerID {
+  @JsonValue('g++')
+  gpp
+}
+
+@JsonSerializable()
 class CompilerConfig {
-  late final String compiler;
+  late final CompilerID compiler;
 
-  late final double capacity;
+  late final int cores;
 
-  late final List<String> entries;
+  late final List<String> sourceDirectories;
 
-  late final List<String> flags;
+  late final List<String> compilerFlags;
 
-  late final Map<String, String> args;
+  late final List<String> linkerFlags;
 
-  late final Map<String, String> trailingArgs;
-
-  CompilerConfig(dynamic json) {
-    compiler = json.compiler;
-    capacity = json.capacity;
-    entries = json.entries;
-    flags = json.flags + json.trailingFlags;
-    args = json.args;
-    trailingArgs = json.trailingArgs;
-
+  CompilerConfig(
+      {required this.compiler,
+      required this.cores,
+      required this.sourceDirectories,
+      required this.compilerFlags,
+      required this.linkerFlags}) {
     _validate();
   }
 
+  factory CompilerConfig.fromJson(Map<String, dynamic> json) => _$CompilerConfigFromJson(json);
+
+  Map<String, dynamic> toJson() => _$CompilerConfigToJson(this);
+
   void _validate() {
-    const invalidOptions = ['c'];
-    const allowedCompilers = ['g++'];
+    const invalidOptions = ['-c'];
 
-    if (!allowedCompilers.contains(compiler)) {
-      final compilers = allowedCompilers.join(',');
-      final message = 'Compiler has to be any of [$compilers]]. Given: "$compiler"';
+    if (cores <= 0) {
+      final message = 'The number od used cores has to be greater than zero. Given: $cores';
       throw Exception(message);
     }
 
-    if (capacity > 1 || capacity <= 0) {
-      final capDesc = capacity.toStringAsFixed(3);
-      final message = 'The capacity has to be in range 0 < capacity <= 1. Given: $capDesc';
-      throw Exception(message);
-    }
-
-    for (final flag in flags) {
-      if (flag.startsWith(RegExp('-(-?)'))) {
-        throw Exception('A flag must not start with "-". Given: $flag');
-      }
-
+    for (var flag in [...compilerFlags, ...linkerFlags]) {
       if (invalidOptions.contains(flag)) {
         throw Exception('"$flag" is not allowed in configuration files.');
       }
-    }
 
-    for (final arg in [...args.entries, ...trailingArgs.entries]) {
-      if (arg.key.startsWith(RegExp('-(-?)'))) {
-        throw Exception('An argument must not start with "-". Given: ${arg.key}');
-      }
-
-      if (invalidOptions.contains(arg.key)) {
-        throw Exception('"${arg.key}" is not allowed in configuration files.');
+      if (!flag.startsWith(RegExp('-(-?)')) && !(flag.startsWith('"') && flag.endsWith('"'))) {
+        flag = '"$flag"';
       }
     }
   }
