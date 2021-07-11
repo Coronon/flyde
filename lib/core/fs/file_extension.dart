b1def1b19d00a7ab6393f74bf503a418a42f9459
Henrik <henrik@thoroee.com>
2021-07-11 19:38:14 +0200

    Add collection for standard file extensions

---
+++ lib/core/fs/file_extension.dart
@@ -0,0 +1,7 @@
+abstract class FileExtension {
+  /// A list of standard extensions for C++ header files
+  static const headers = ['.h', '.hpp', '.hh', '.h++'];
+
+  /// A list of standard extensions for C++ source files
+  static const sources = ['.c', '.cpp', '.cc', '.c++'];
+}
