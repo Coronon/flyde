cd38b291d4191f47afcc129428327a291f50cdf6
Henrik <henrik@thoroee.com>
2021-07-13 16:39:14 +0200

    Support library and app library path

--- lib/core/fs/standard_location.dart
+++ lib/core/fs/standard_location.dart
@@ -1,12 +1,22 @@
 import 'dart:io';
 
-enum StandardLocation { tmp }
+import 'package:path/path.dart';
+
+enum StandardLocation { tmp, library, applicationLibrary }
 
 extension StandardLocationImpl on StandardLocation {
   Directory get directory {
     switch (this) {
       case StandardLocation.tmp:
         return Directory.systemTemp;
+      case StandardLocation.library:
+        return Directory('/var/lib');
+      case StandardLocation.applicationLibrary:
+        if (Platform.environment.containsKey('FLYDE_APPLIBRARY')) {
+          return Directory(Platform.environment['FLYDE_APPLIBRARY'] as String);
+        }
+
+        return Directory(join(StandardLocation.library.directory.path, 'flyde'));
     }
   }
 }
