19d4a7e6e3aa830a4dde1d168e4cb76be994d051
Rubin Raithel <33808743+Coronon@users.noreply.github.com>
2021-07-15 01:15:28 +0200

    (Test + Networking) remove unnecessary groups

--- test/core/networking/protocol/protocol_test.dart
+++ test/core/networking/protocol/protocol_test.dart
@@ -7,78 +7,76 @@ void main() {
   ProtocolDelegate.elements['MockSerializable'] =
       (Map<String, dynamic> json) => MockSerializable.fromJson(json);
 
-  group('Serialization', () {
-    test('ProtocolDelegate can recreate object', () {
-      MockSerializable serializable = MockSerializable("ANYTHING");
+  test('ProtocolDelegate can recreate object', () {
+    MockSerializable serializable = MockSerializable("ANYTHING");
 
-      dynamic recreated = ProtocolDelegate.deSerialize(ProtocolDelegate.serialize(serializable));
+    dynamic recreated = ProtocolDelegate.deSerialize(ProtocolDelegate.serialize(serializable));
 
-      expect(recreated is MockSerializable, equals(true));
-      recreated as MockSerializable;
-      expect(recreated.value, equals(serializable.value));
-    });
+    expect(recreated is MockSerializable, equals(true));
+    recreated as MockSerializable;
+    expect(recreated.value, equals(serializable.value));
+  });
 
-    test('ProtocolDelegate can serialize object', () {
-      MockSerializable serializable = MockSerializable("ANYTHING");
+  test('ProtocolDelegate can serialize object', () {
+    MockSerializable serializable = MockSerializable("ANYTHING");
 
-      String serialized = ProtocolDelegate.serialize(serializable);
+    String serialized = ProtocolDelegate.serialize(serializable);
 
-      expect(serialized, equals('{"type":"MockSerializable","data":{"value":"ANYTHING"}}'));
-    });
+    expect(serialized, equals('{"type":"MockSerializable","data":{"value":"ANYTHING"}}'));
+  });
 
-    test('ProtocolDelegate can deserialize object', () {
-      String serialized = '{"type":"MockSerializable","data":{"value":"ANYTHING"}}';
+  test('ProtocolDelegate can deserialize object', () {
+    String serialized = '{"type":"MockSerializable","data":{"value":"ANYTHING"}}';
 
-      dynamic deSerialized = ProtocolDelegate.deSerialize(serialized);
+    dynamic deSerialized = ProtocolDelegate.deSerialize(serialized);
 
-      expect(deSerialized is MockSerializable, equals(true));
-      deSerialized as MockSerializable;
-      expect(deSerialized.value, equals("ANYTHING"));
-    });
+    expect(deSerialized is MockSerializable, equals(true));
+    deSerialized as MockSerializable;
+    expect(deSerialized.value, equals("ANYTHING"));
+  });
 
-    test('ProtocolDelegate throw if no type provided', () {
-      String msg = '{"data": {"value": "MockSerializable"}}';
+  test('ProtocolDelegate throw if no type provided', () {
+    String msg = '{"data": {"value": "MockSerializable"}}';
 
-      expect(
-        () => ProtocolDelegate.deSerialize(msg),
-        throwsA(
-          isA<ProtocolException>().having(
-            (ProtocolException error) => error.message,
-            'message',
-            equals("Message does not contain 'type' property"),
-          ),
+    expect(
+      () => ProtocolDelegate.deSerialize(msg),
+      throwsA(
+        isA<ProtocolException>().having(
+          (ProtocolException error) => error.message,
+          'message',
+          equals("Message does not contain 'type' property"),
         ),
-      );
-    });
+      ),
+    );
+  });
 
-    test('ProtocolDelegate throw if invalid type provided', () {
-      String msg = '{"type": "NonExistentType", "data": {"value": "MockSerializable"}}';
+  test('ProtocolDelegate throw if invalid type provided', () {
+    String msg = '{"type": "NonExistentType", "data": {"value": "MockSerializable"}}';
 
-      expect(
-        () => ProtocolDelegate.deSerialize(msg),
-        throwsA(
-          isA<ProtocolException>().having(
-            (ProtocolException error) => error.message,
-            'message',
-            equals("'NonExistentType' unknown protocol element"),
-          ),
+    expect(
+      () => ProtocolDelegate.deSerialize(msg),
+      throwsA(
+        isA<ProtocolException>().having(
+          (ProtocolException error) => error.message,
+          'message',
+          equals("'NonExistentType' unknown protocol element"),
         ),
-      );
-    });
+      ),
+    );
+  });
 
-    test('ProtocolDelegate throw if invalid data', () {
-      String msg1 = '{"type": "MockSerializable", "data": {"value": 1}}';
-      String msg2 = '{"type": "MockSerializable", "data": {"ANYTHING": "ANYTHING"}}';
+  test('ProtocolDelegate throw if invalid data', () {
+    String msg1 = '{"type": "MockSerializable", "data": {"value": 1}}';
+    String msg2 = '{"type": "MockSerializable", "data": {"ANYTHING": "ANYTHING"}}';
 
-      expect(() => ProtocolDelegate.deSerialize(msg1), throwsA(isA<TypeError>()));
-      expect(() => ProtocolDelegate.deSerialize(msg2), throwsA(isA<TypeError>()));
-    });
+    expect(() => ProtocolDelegate.deSerialize(msg1), throwsA(isA<TypeError>()));
+    expect(() => ProtocolDelegate.deSerialize(msg2), throwsA(isA<TypeError>()));
+  });
 
-    test('ProtocolDelegate throw if not json string', () {
-      String msg = 'ANYTHING';
+  test('ProtocolDelegate throw if not json string', () {
+    String msg = 'ANYTHING';
 
-      expect(() => ProtocolDelegate.deSerialize(msg), throwsA(isA<FormatException>()));
-    });
+    expect(() => ProtocolDelegate.deSerialize(msg), throwsA(isA<FormatException>()));
   });
 }
 
--- test/core/networking/websockets/middleware_test.dart
+++ test/core/networking/websockets/middleware_test.dart
@@ -6,252 +6,250 @@ import 'package:flyde/core/networking/websockets/middleware.dart';
 import '../../../helpers/value_hook.dart';
 
 void main() {
-  group('WebSocket middleware:', () {
-    test('ProtocolMiddleware deserializes messages and passes them onwards', () async {
-      AuthRequest request = AuthRequest(username: "testUsername", password: "testPassword");
-      String message =
-          '{"type":"AuthRequest","data":{"username":"testUsername","password":"testPassword"}}';
-      VHook<bool> calledNext = VHook<bool>(false);
+  test('ProtocolMiddleware deserializes messages and passes them onwards', () async {
+    AuthRequest request = AuthRequest(username: "testUsername", password: "testPassword");
+    String message =
+        '{"type":"AuthRequest","data":{"username":"testUsername","password":"testPassword"}}';
+    VHook<bool> calledNext = VHook<bool>(false);
 
-      dynamic deserialized = await protocolMiddleware(
-        null,
-        message,
-        MiddlewareAction.recieve,
-        (dynamic msg) async {
-          expect(msg is AuthRequest, equals(true));
+    dynamic deserialized = await protocolMiddleware(
+      null,
+      message,
+      MiddlewareAction.recieve,
+      (dynamic msg) async {
+        expect(msg is AuthRequest, equals(true));
 
-          calledNext.set(true);
-          return msg;
-        },
-      );
+        calledNext.set(true);
+        return msg;
+      },
+    );
 
-      //* Verify that the deserialized message is the same as the original message
-      expect(deserialized is AuthRequest, equals(true));
-      deserialized as AuthRequest;
+    //* Verify that the deserialized message is the same as the original message
+    expect(deserialized is AuthRequest, equals(true));
+    deserialized as AuthRequest;
 
-      expect(
-        deserialized.username,
-        equals(request.username),
-      );
-      expect(
-        deserialized.password,
-        equals(request.password),
-      );
+    expect(
+      deserialized.username,
+      equals(request.username),
+    );
+    expect(
+      deserialized.password,
+      equals(request.password),
+    );
 
-      calledNext.expect(equals(true));
+    calledNext.expect(equals(true));
+  });
+  test('ProtocolMiddleware passes messages and finally serializes them', () async {
+    AuthRequest request = AuthRequest(username: "testUsername", password: "testPassword");
+    String message =
+        '{"type":"AuthRequest","data":{"username":"testUsername","password":"testPassword"}}';
+    VHook<bool> calledNext = VHook<bool>(false);
+
+    dynamic serialized = await protocolMiddleware(
+      null,
+      request,
+      MiddlewareAction.send,
+      (dynamic msg) async {
+        // Should still be non serialized
+        expect(msg is AuthRequest, equals(true));
+
+        calledNext.set(true);
+        return msg;
+      },
+    );
+
+    //* Verify that the serialized message is the same as the original message
+    expect(serialized, equals(message));
+
+    calledNext.expect(equals(true));
+  });
+  test('ProtocolMiddleware catches exceptions', () async {
+    MockSession session = MockSession();
+    VHook<bool> calledNext = VHook<bool>(false);
+
+    dynamic serialized = await protocolMiddleware(
+      session,
+      "ANYTHING",
+      MiddlewareAction.recieve,
+      (dynamic msg) async {
+        calledNext.set(true);
+        return msg;
+      },
+    );
+
+    //* Verify that the middleware caugth the exception
+    expect(serialized, equals(null));
+
+    calledNext.expect(equals(false));
+  });
+
+  test('AuthenticationMiddleware dont run on send', () async {
+    MockSession session = MockSession();
+    VHook<bool> calledAuthHandler = VHook<bool>(false);
+    VHook<bool> calledNext = VHook<bool>(false);
+
+    MiddlewareFunc authMiddleware = makeAuthenticationMiddleware((AuthRequest request) async {
+      calledAuthHandler.set(true);
+      return true;
     });
-    test('ProtocolMiddleware passes messages and finally serializes them', () async {
-      AuthRequest request = AuthRequest(username: "testUsername", password: "testPassword");
-      String message =
-          '{"type":"AuthRequest","data":{"username":"testUsername","password":"testPassword"}}';
-      VHook<bool> calledNext = VHook<bool>(false);
 
-      dynamic serialized = await protocolMiddleware(
-        null,
-        request,
-        MiddlewareAction.send,
-        (dynamic msg) async {
-          // Should still be non serialized
-          expect(msg is AuthRequest, equals(true));
+    dynamic response = await authMiddleware(
+      session,
+      null,
+      MiddlewareAction.send,
+      (dynamic msg) async {
+        calledNext.set(true);
+        return msg;
+      },
+    );
 
-          calledNext.set(true);
-          return msg;
-        },
-      );
+    //* Verify that the middleware did not intercept the message
+    calledAuthHandler.expect(equals(false));
+    calledNext.expect(equals(true));
+    expect(response == null, equals(true));
+    expect(session.storage['authenticated'], equals(null));
+  });
+  test('AuthenticationMiddleware requires authentication', () async {
+    MockSession session = MockSession();
+    VHook<bool> calledAuthHandler = VHook<bool>(false);
+    VHook<bool> calledNext = VHook<bool>(false);
 
-      //* Verify that the serialized message is the same as the original message
-      expect(serialized, equals(message));
-
-      calledNext.expect(equals(true));
-    });
-    test('ProtocolMiddleware catches exceptions', () async {
-      MockSession session = MockSession();
-      VHook<bool> calledNext = VHook<bool>(false);
-
-      dynamic serialized = await protocolMiddleware(
-        session,
-        "ANYTHING",
-        MiddlewareAction.recieve,
-        (dynamic msg) async {
-          calledNext.set(true);
-          return msg;
-        },
-      );
-
-      //* Verify that the middleware caugth the exception
-      expect(serialized, equals(null));
-
-      calledNext.expect(equals(false));
+    MiddlewareFunc authMiddleware = makeAuthenticationMiddleware((AuthRequest request) async {
+      calledAuthHandler.set(true);
+      return true;
     });
 
-    test('AuthenticationMiddleware dont run on send', () async {
-      MockSession session = MockSession();
-      VHook<bool> calledAuthHandler = VHook<bool>(false);
-      VHook<bool> calledNext = VHook<bool>(false);
+    dynamic response = await authMiddleware(
+      session,
+      "ANYTHING",
+      MiddlewareAction.recieve,
+      (dynamic msg) async {
+        calledNext.set(true);
+        return msg;
+      },
+    );
 
-      MiddlewareFunc authMiddleware = makeAuthenticationMiddleware((AuthRequest request) async {
-        calledAuthHandler.set(true);
-        return true;
-      });
+    //* Verify that the middleware rejected the message
+    calledAuthHandler.expect(equals(false));
+    calledNext.expect(equals(false));
+    expect(response is AuthResponse, equals(true));
+    response as AuthResponse;
+    expect(response.status, equals(AuthResponseStatus.required));
+    expect(session.storage['authenticated'], equals(null));
+  });
+  test('AuthenticationMiddleware lets authed sessions pass', () async {
+    MockSession session = MockSession();
+    session.storage['authenticated'] = true;
+    VHook<bool> calledAuthHandler = VHook<bool>(false);
+    VHook<bool> calledNext = VHook<bool>(false);
 
-      dynamic response = await authMiddleware(
-        session,
-        null,
-        MiddlewareAction.send,
-        (dynamic msg) async {
-          calledNext.set(true);
-          return msg;
-        },
-      );
-
-      //* Verify that the middleware did not intercept the message
-      calledAuthHandler.expect(equals(false));
-      calledNext.expect(equals(true));
-      expect(response == null, equals(true));
-      expect(session.storage['authenticated'], equals(null));
+    MiddlewareFunc authMiddleware = makeAuthenticationMiddleware((AuthRequest request) async {
+      calledAuthHandler.set(true);
+      return false;
     });
-    test('AuthenticationMiddleware requires authentication', () async {
-      MockSession session = MockSession();
-      VHook<bool> calledAuthHandler = VHook<bool>(false);
-      VHook<bool> calledNext = VHook<bool>(false);
 
-      MiddlewareFunc authMiddleware = makeAuthenticationMiddleware((AuthRequest request) async {
-        calledAuthHandler.set(true);
-        return true;
-      });
+    dynamic response = await authMiddleware(
+      session,
+      "ANYTHING",
+      MiddlewareAction.recieve,
+      (dynamic msg) async {
+        expect(msg, equals("ANYTHING"));
 
-      dynamic response = await authMiddleware(
-        session,
-        "ANYTHING",
-        MiddlewareAction.recieve,
-        (dynamic msg) async {
-          calledNext.set(true);
-          return msg;
-        },
-      );
+        calledNext.set(true);
+        return msg;
+      },
+    );
 
-      //* Verify that the middleware rejected the message
-      calledAuthHandler.expect(equals(false));
-      calledNext.expect(equals(false));
-      expect(response is AuthResponse, equals(true));
-      response as AuthResponse;
-      expect(response.status, equals(AuthResponseStatus.required));
-      expect(session.storage['authenticated'], equals(null));
+    //* Verify that the middleware let the message through
+    calledAuthHandler.expect(equals(false));
+    calledNext.expect(equals(true));
+    expect(response is String, equals(true));
+    response as String;
+    expect(response, equals("ANYTHING"));
+  });
+  test('AuthenticationMiddleware can authentication', () async {
+    MockSession session = MockSession();
+    VHook<bool> calledAuthHandler = VHook<bool>(false);
+    VHook<bool> calledNext = VHook<bool>(false);
+
+    MiddlewareFunc authMiddleware = makeAuthenticationMiddleware((AuthRequest request) async {
+      expect(request.username, equals("testUsername"));
+      expect(request.password, equals("testPassword"));
+
+      calledAuthHandler.set(true);
+      return true;
     });
-    test('AuthenticationMiddleware lets authed sessions pass', () async {
-      MockSession session = MockSession();
-      session.storage['authenticated'] = true;
-      VHook<bool> calledAuthHandler = VHook<bool>(false);
-      VHook<bool> calledNext = VHook<bool>(false);
 
-      MiddlewareFunc authMiddleware = makeAuthenticationMiddleware((AuthRequest request) async {
-        calledAuthHandler.set(true);
-        return false;
-      });
+    dynamic response = await authMiddleware(
+      session,
+      AuthRequest(username: "testUsername", password: "testPassword"),
+      MiddlewareAction.recieve,
+      (dynamic msg) async {
+        calledNext.set(true);
+        return msg;
+      },
+    );
 
-      dynamic response = await authMiddleware(
-        session,
-        "ANYTHING",
-        MiddlewareAction.recieve,
-        (dynamic msg) async {
-          expect(msg, equals("ANYTHING"));
+    //* Verify that the middleware can accept AuthRequest's
+    calledAuthHandler.expect(equals(true));
+    calledNext.expect(equals(false));
+    expect(response is AuthResponse, equals(true));
+    response as AuthResponse;
+    expect(response.status, equals(AuthResponseStatus.success));
+    expect(session.storage['authenticated'], equals(true));
+  });
+  test('AuthenticationMiddleware can reject', () async {
+    MockSession session = MockSession();
+    VHook<bool> calledAuthHandler = VHook<bool>(false);
+    VHook<bool> calledNext = VHook<bool>(false);
 
-          calledNext.set(true);
-          return msg;
-        },
-      );
+    MiddlewareFunc authMiddleware = makeAuthenticationMiddleware((AuthRequest request) async {
+      expect(request.username, equals("testUsername"));
+      expect(request.password, equals("testPassword"));
 
-      //* Verify that the middleware let the message through
-      calledAuthHandler.expect(equals(false));
-      calledNext.expect(equals(true));
-      expect(response is String, equals(true));
-      response as String;
-      expect(response, equals("ANYTHING"));
+      calledAuthHandler.set(true);
+      return false;
     });
-    test('AuthenticationMiddleware can authentication', () async {
-      MockSession session = MockSession();
-      VHook<bool> calledAuthHandler = VHook<bool>(false);
-      VHook<bool> calledNext = VHook<bool>(false);
 
-      MiddlewareFunc authMiddleware = makeAuthenticationMiddleware((AuthRequest request) async {
-        expect(request.username, equals("testUsername"));
-        expect(request.password, equals("testPassword"));
+    dynamic response1 = await authMiddleware(
+      session,
+      AuthRequest(username: "testUsername", password: "testPassword"),
+      MiddlewareAction.recieve,
+      (dynamic msg) async {
+        calledNext.set(true);
+        return msg;
+      },
+    );
 
-        calledAuthHandler.set(true);
-        return true;
-      });
+    //* Verify that the middleware can reject AuthRequest's
+    calledAuthHandler.expect(equals(true));
+    calledNext.expect(equals(false));
+    expect(response1 is AuthResponse, equals(true));
+    response1 as AuthResponse;
+    expect(response1.status, equals(AuthResponseStatus.failure));
+    expect(session.storage['authenticated'], equals(false));
 
-      dynamic response = await authMiddleware(
-        session,
-        AuthRequest(username: "testUsername", password: "testPassword"),
-        MiddlewareAction.recieve,
-        (dynamic msg) async {
-          calledNext.set(true);
-          return msg;
-        },
-      );
+    //? Second attempt (session.storage['authenticated'] is now not 'null')
+    calledAuthHandler.set(false);
+    calledNext.set(false);
 
-      //* Verify that the middleware can accept AuthRequest's
-      calledAuthHandler.expect(equals(true));
-      calledNext.expect(equals(false));
-      expect(response is AuthResponse, equals(true));
-      response as AuthResponse;
-      expect(response.status, equals(AuthResponseStatus.success));
-      expect(session.storage['authenticated'], equals(true));
-    });
-    test('AuthenticationMiddleware can reject', () async {
-      MockSession session = MockSession();
-      VHook<bool> calledAuthHandler = VHook<bool>(false);
-      VHook<bool> calledNext = VHook<bool>(false);
+    dynamic response2 = await authMiddleware(
+      session,
+      AuthRequest(username: "testUsername", password: "testPassword"),
+      MiddlewareAction.recieve,
+      (dynamic msg) async {
+        calledNext.set(true);
+        return msg;
+      },
+    );
 
-      MiddlewareFunc authMiddleware = makeAuthenticationMiddleware((AuthRequest request) async {
-        expect(request.username, equals("testUsername"));
-        expect(request.password, equals("testPassword"));
-
-        calledAuthHandler.set(true);
-        return false;
-      });
-
-      dynamic response1 = await authMiddleware(
-        session,
-        AuthRequest(username: "testUsername", password: "testPassword"),
-        MiddlewareAction.recieve,
-        (dynamic msg) async {
-          calledNext.set(true);
-          return msg;
-        },
-      );
-
-      //* Verify that the middleware can reject AuthRequest's
-      calledAuthHandler.expect(equals(true));
-      calledNext.expect(equals(false));
-      expect(response1 is AuthResponse, equals(true));
-      response1 as AuthResponse;
-      expect(response1.status, equals(AuthResponseStatus.failure));
-      expect(session.storage['authenticated'], equals(false));
-
-      //? Second attempt (session.storage['authenticated'] is now not 'null')
-      calledAuthHandler.set(false);
-      calledNext.set(false);
-
-      dynamic response2 = await authMiddleware(
-        session,
-        AuthRequest(username: "testUsername", password: "testPassword"),
-        MiddlewareAction.recieve,
-        (dynamic msg) async {
-          calledNext.set(true);
-          return msg;
-        },
-      );
-
-      //* Verify that the middleware can reject AuthRequest's
-      calledAuthHandler.expect(equals(true));
-      calledNext.expect(equals(false));
-      expect(response2 is AuthResponse, equals(true));
-      response2 as AuthResponse;
-      expect(response2.status, equals(AuthResponseStatus.failure));
-      expect(session.storage['authenticated'], equals(false));
-    });
+    //* Verify that the middleware can reject AuthRequest's
+    calledAuthHandler.expect(equals(true));
+    calledNext.expect(equals(false));
+    expect(response2 is AuthResponse, equals(true));
+    response2 as AuthResponse;
+    expect(response2.status, equals(AuthResponseStatus.failure));
+    expect(session.storage['authenticated'], equals(false));
   });
 }
 
