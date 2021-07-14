162161aa84ee970b61bb1aeea6a9243b4eea2756
Rubin Raithel <33808743+Coronon@users.noreply.github.com>
2021-07-14 17:11:58 +0200

    (Test + Helpers) add awaitValue method to VHook

--- test/helpers/value_hook.dart
+++ test/helpers/value_hook.dart
@@ -1,7 +1,9 @@
+import 'dart:async';
+
 import 'package:test/test.dart' as test show expect, Matcher;
 
 /// Useful to easily test handler functions
-///  to set values outside of their scope.
+///  that set values outside of their scope.
 ///
 /// ```dart
 /// // Construct VHook with initial value of 'false'
@@ -44,4 +46,65 @@ class VHook<T> {
         reason: reason,
         skip: skip,
       );
+
+  /// Get awaitable that completes when the contained value is non-null.
+  ///
+  /// Note: The initial value of the VHook must have been null, and the final
+  /// value non-null.
+  ///
+  /// The optional [timeout] specifies the maximum time to wait for the value.
+  /// [pollInterval] specifies the time between value checks.
+  /// If [raiseOnTimeout] is true, this function will throw if the timeout is reached.
+  ///
+  /// The returned Future's completion value is whether the value was set (true)
+  /// or whether the timeout was reached (false).
+  ///
+  /// ```dart
+  /// // Construct VHook with initial value of 'null'
+  /// VHook<bool?> conditionValue = VHook<bool?>(null);
+  ///
+  /// // This function is supposed to call the handler soon
+  /// willCallHandlerAsync((){
+  ///   if (someCondition) {
+  ///     conditionValue.set(true);
+  ///   } else {
+  ///     conditionValue.set(false);
+  ///   }
+  /// });
+  ///
+  /// // Wait for the handler to be called
+  /// // If the handler was not called after 5s, throw an error
+  /// await conditionValue.awaitValue(Duration(seconds: 5), raiseOnTimeout: true);
+  ///
+  /// // This will throw an error if the condition evaluated to false
+  /// conditionValue.expect(equals(true));
+  /// ```
+  Future<bool> awaitValue(
+    Duration? timeout, [
+    Duration pollInterval = Duration.zero,
+    bool raiseOnTimeout = false,
+  ]) async {
+    // Handle optional timeout
+    DateTime? expireTime;
+    if (timeout != null) {
+      expireTime = DateTime.now().add(timeout);
+    }
+
+    Completer<bool> completer = Completer<bool>();
+    // Check if the value has changed
+    check() {
+      if (value != null) {
+        completer.complete(true);
+      } else if (expireTime != null && DateTime.now().isAfter(expireTime)) {
+        if (raiseOnTimeout) throw TimeoutException("Timed out awaiting value");
+
+        completer.complete(false);
+      } else {
+        Timer(pollInterval, check);
+      }
+    }
+
+    check();
+    return completer.future;
+  }
 }
