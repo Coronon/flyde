13cb164592ff655ca41c608d96b610952182cf18
Henrik <henrik@thoroee.com>
2021-07-15 13:09:50 +0200

    Test `ProjectCache`

---
+++ test/features/build_server/cache/project_cache_test.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+import 'package:flyde/features/build_server/cache/project_cache.dart';
+import 'package:test/test.dart';
+
+import '../../../helpers/clear_test_cache_directory.dart';
+import '../../../helpers/create_dummy_project_cache.dart';
+
+const _cacheId = 'project_cache_test';
+const _dummyProjectCachePath = './flyde-test-lib-$_cacheId/cache/testing';
+
+void main() {
+  setUp(() async => await clearTestCacheDirectory(id: _cacheId));
+  tearDown(() async => await clearTestCacheDirectory(id: _cacheId));
+
+  test('Creates lock file on init', () async {
+    await createDummyProjectCache(id: _cacheId);
+
+    final lock = File('$_dummyProjectCachePath/.lock.json');
+    final exists = await lock.exists();
+
+    expect(exists, true);
+  });
+
+  test('Can handle blank cache', () async {
+    await createDummyProjectCache(id: _cacheId);
+    await expectLater(createDummyProjectCache(id: _cacheId), completion(isA<ProjectCache>()));
+  });
+}
