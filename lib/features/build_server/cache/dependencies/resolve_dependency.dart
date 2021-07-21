f28194a7cd97621ec6c686251e9905c13ebdbbeb
Henrik <henrik@thoroee.com>
2021-07-21 22:32:49 +0200

    Add method to resolve the id of a dependency

---
+++ lib/features/build_server/cache/dependencies/resolve_dependency.dart
@@ -0,0 +1,40 @@
+import 'dart:io';
+
+import 'package:flyde/core/fs/wrapper/source_file.dart';
+import 'package:path/path.dart';
+
+/// Resolves the [dependency] of [origin], which is located within [entryDirectory], and returns
+/// the id of the resolved dependency.
+///
+/// If [dependency] is an absolute name, `resolve` checks if it exists exactly one time within [all] source files.
+Future<String> resolve(
+  String dependency,
+  SourceFile origin,
+  List<SourceFile> all,
+  Directory entryDirectory,
+) async {
+  final originPath = origin.relativePath(filename: false);
+
+  if (RegExp(r'^.*(\/|\\).+$').hasMatch(dependency)) {
+    // Dependency is a relative path to origin -> resolve path and check if the file exists
+    final resolved = normalize(join(originPath, dependency));
+    final file = File(normalize(join(entryDirectory.path, resolved)));
+
+    if (!await file.exists() || !isWithin(entryDirectory.path, file.path)) {
+      throw ArgumentError('Cannot resolve dependency "$dependency" to an existing file.');
+    }
+
+    return SourceFile.fromFile(origin.entry, file, entryDirectory: entryDirectory).id;
+  }
+
+  // Dependency is an absolute name
+  // -> iterate over all files and check if the name and extension exist at the entry point of [origin]
+  final name = withoutExtension(dependency);
+  final extension = context.extension(dependency).substring(1);
+
+  return all
+      .singleWhere(
+        (file) => file.name == name && file.extension == extension && file.entry == origin.entry,
+      )
+      .id;
+}
