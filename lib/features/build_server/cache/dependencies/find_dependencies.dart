d28b35a96f4698f486e5462ecc0f60df9b080741
Henrik <henrik@thoroee.com>
2021-07-21 19:56:28 +0200

    Add function which returns all C++ file includes

---
+++ lib/features/build_server/cache/dependencies/find_dependencies.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:flyde/core/fs/wrapper/source_file.dart';
+
+/// Find all dependencies in [file] and returns them as found in [file].
+Future<Set<String>> findDependencies(SourceFile file) async {
+  final text = utf8.decode((await file.data).toList());
+  final includes = LineSplitter()
+      .convert(text)
+      // Remove comments
+      .map((line) => line.replaceAll(RegExp(r'\/\/.*$'), ""))
+      // Remove leading and trailing whitespace
+      .map((line) => line.trim())
+      // Get all possible includes
+      .where((line) => line.startsWith('#include'))
+      // Remove '#include' prefix
+      .map((line) => line.substring('#include'.length))
+      // Filter out all <*> includes and ensure "*" or '*'
+      .where((include) =>
+          !RegExp(r'^\<[^\0]+\>$').hasMatch(include) &&
+          (RegExp(r'^\"[^\0]+\"$').hasMatch(include) || RegExp(r"^\'[^\0]+\'$").hasMatch(include)))
+      // Remove quotes
+      .map((include) => include.substring(1, include.length - 1))
+      .toSet();
+
+  return includes;
+}
