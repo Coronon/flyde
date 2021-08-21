247f8936570972f7273804e1962971e15a7e4381
Henrik <henrik@thoroee.com>
2021-08-21 13:14:12 +0200

    Test full build process

--- test/features/build_server/build_provider_test.dart
+++ test/features/build_server/build_provider_test.dart
@@ -1,12 +1,15 @@
 import 'dart:async';
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:flyde/core/fs/compiler/installed_compiler.dart';
 import 'package:flyde/core/fs/configs/compiler_config.dart';
 import 'package:flyde/core/fs/file_extension.dart';
 import 'package:flyde/core/fs/search_directory.dart';
 import 'package:flyde/core/fs/wrapper/source_file.dart';
+import 'package:flyde/core/networking/protocol/compile_status.dart';
 import 'package:flyde/core/networking/protocol/process_completion.dart';
+import 'package:flyde/core/networking/protocol/project_build.dart';
 import 'package:flyde/core/networking/protocol/project_update.dart';
 import 'package:flyde/core/networking/protocol/project_init.dart';
 import 'package:flyde/core/networking/server.dart';
@@ -20,6 +23,98 @@ import '../../helpers/clear_test_cache_directory.dart';
 import '../../helpers/get_uri.dart';
 import '../../helpers/value_hook.dart';
 
+/// Name of the temporary file storage
+const tmpDirName = 'flyde-test-lib-build_provider_test_binary';
+
+/// Verifies that a prject can be built and the binary can be downloaded using the 'test' package.
+///
+/// The downloaded binary is returned if the build was successful.
+/// Otherwise the tests will fail.
+Future<Uint8List> _requestAndDownloadProject(
+  String projectName,
+  ClientSession clientSession,
+  Map<String, String> fileMap,
+  List<SourceFile> files,
+  CompilerConfig config,
+) async {
+  List<String> requiredFiles = [];
+  final isDone = VHook<bool?>(null);
+  final hasBinary = VHook<bool?>(null);
+  Uint8List? bin;
+
+  clientSession.onMessage = (session, message) async {
+    if (message is ProjectUpdateResponse) {
+      requiredFiles = message.files;
+
+      final id = requiredFiles.removeLast();
+      final file = files.singleWhere((f) => f.id == id);
+
+      await session.send(FileUpdate.fromSourceFile(file));
+    }
+
+    if (message is CompileStatusMessage) {
+      if (message.status == CompileStatus.done) {
+        isDone.set(true);
+      }
+    }
+
+    if (message is ProcessCompletionMessage) {
+      if (message.process == CompletableProcess.projectInit) {
+        await clientSession.send(ProjectUpdateRequest(config: config, files: fileMap));
+      }
+
+      if (message.process == CompletableProcess.fileUpdate) {
+        if (requiredFiles.isNotEmpty) {
+          final id = requiredFiles.removeLast();
+          final file = files.singleWhere((f) => f.id == id);
+
+          await session.send(FileUpdate.fromSourceFile(file));
+        } else {
+          await session.send(projectBuildRequest);
+        }
+      }
+    }
+
+    if (message is BinaryResponse) {
+      hasBinary.set(message.binary != null);
+      bin = message.binary;
+    }
+  };
+
+  await clientSession.send(ProjectInitRequest(id: projectName, name: projectName));
+
+  await isDone.awaitValue(Duration(seconds: 5), raiseOnTimeout: true);
+  isDone.expect(equals(true));
+
+  await clientSession.send(getBinaryRequest);
+  await hasBinary.awaitValue(Duration(seconds: 5), raiseOnTimeout: true);
+  hasBinary.expect(equals(true));
+
+  return bin!;
+}
+
+/// Runs the binary and compares the output to the expected output.
+Future<void> _runBinary(Uint8List binary, String fileName, String expectedOutput) async {
+  final dir = Directory('./$tmpDirName');
+  final file = File('./$tmpDirName/$fileName.out');
+
+  // Create and write binary to disk
+  await dir.create();
+  await file.create();
+  await file.writeAsBytes(binary.toList());
+
+  // Make the binary runable
+  await Process.run('chmod', ['u+x', file.path]);
+
+  // Run the binary
+  final proc = await Process.run(file.path, []);
+  expect(proc.stdout.trim(), equals(expectedOutput));
+
+  // Clean up
+  await file.delete();
+  await dir.delete();
+}
+
 void main() async {
   const String cacheSuffix = 'build_provider';
   final cacheDir = Directory('./flyde-test-lib-$cacheSuffix');
@@ -28,6 +123,13 @@ void main() async {
     threads: 4,
     sourceDirectories: ['./example'],
     compilerFlags: ['-O2', '-DPRINTHELLO'],
+    linkerFlags: ['-flto'],
+  );
+  final config2 = CompilerConfig(
+    compiler: InstalledCompiler.gpp,
+    threads: 4,
+    sourceDirectories: ['./example'],
+    compilerFlags: ['-O3', '-DPRINTBYE'],
     linkerFlags: ['-flto'],
   );
 
@@ -120,5 +222,44 @@ void main() async {
     //? Wait for 500ms to ensure no errors have been thrown.
     await hadError.awaitValue(Duration(milliseconds: 500));
     hadError.expect(isNot(equals(true)));
+  });
+
+  test('Receives correct binary after successfull build', () async {
+    final Uint8List bin = await _requestAndDownloadProject(
+      'test',
+      clientSession,
+      fileMap,
+      files,
+      config1,
+    );
+
+    await _runBinary(bin, 'test', 'HELLO');
+  });
+
+  test('Can handle multiple clients', () async {
+    final secondSession = ClientSession(getUri(server, 'ws').toString())
+      ..middleware.add(protocolMiddleware);
+
+    final List<Uint8List> bins = await Future.wait([
+      _requestAndDownloadProject(
+        'test',
+        clientSession,
+        fileMap,
+        files,
+        config1,
+      ),
+      _requestAndDownloadProject(
+        'test1',
+        secondSession,
+        fileMap,
+        files,
+        config2,
+      ),
+    ]);
+
+    await _runBinary(bins[0], 'test', 'HELLO');
+    await _runBinary(bins[1], 'test1', 'BYE');
+
+    secondSession.close();
   });
 }
