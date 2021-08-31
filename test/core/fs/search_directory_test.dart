504598fbdd9de9d7091d4a218e3bba738b6a0b24
Henrik <henrik@thoroee.com>
2021-08-31 18:01:05 +0200

    Test `searchDirectory`

---
+++ test/core/fs/search_directory_test.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+import 'package:flyde/core/fs/search_directory.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('Can list all files', () async {
+    final files = await searchDirectory(Directory('example'), fileMatcher);
+    expect(
+      files.map((e) => e.path),
+      unorderedEquals([
+        'example/.vscode/c_cpp_properties.json',
+        'example/.vscode/settings.json',
+        'example/src/main.cpp',
+        'example/include/constants/numbers.hpp',
+        'example/include/model/Calculator.hpp',
+        'example/src/model/Calculator.cpp',
+      ]),
+    );
+  });
+
+  test('Returns empty list on non-existing directory', () async {
+    expect(
+      await searchDirectory(Directory('not-real'), fileMatcher),
+      isEmpty,
+    );
+  });
+}
