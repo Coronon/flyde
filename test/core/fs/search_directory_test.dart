fe4cdfa0e1aa619676e37e26d41160b01c8036d4
Henrik <henrik@thoroee.com>
2021-09-07 13:09:13 +0200

    Use path.hash to compare files

--- test/core/fs/search_directory_test.dart
+++ test/core/fs/search_directory_test.dart
@@ -1,21 +1,24 @@
 import 'dart:io';
 
+import 'package:path/path.dart' as p;
 import 'package:test/test.dart';
 import 'package:flyde/core/fs/search_directory.dart';
 
 void main() {
   test('Can list all files', () async {
     final files = await searchDirectory(Directory('example'), fileMatcher);
+    final List<String> expected = [
+      'example/.vscode/c_cpp_properties.json',
+      'example/.vscode/settings.json',
+      'example/src/main.cpp',
+      'example/include/constants/numbers.hpp',
+      'example/include/model/Calculator.hpp',
+      'example/src/model/Calculator.cpp',
+    ];
+
     expect(
-      files.map((e) => e.path),
-      unorderedEquals([
-        'example/.vscode/c_cpp_properties.json',
-        'example/.vscode/settings.json',
-        'example/src/main.cpp',
-        'example/include/constants/numbers.hpp',
-        'example/include/model/Calculator.hpp',
-        'example/src/model/Calculator.cpp',
-      ]),
+      files.map((f) => p.hash(f.path)),
+      unorderedEquals(expected.map((f) => p.hash(f))),
     );
   });
 
