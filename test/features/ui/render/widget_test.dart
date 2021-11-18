254d854f1f4e56aad941fd73162b8771af57269a
Henrik <henrik@thoroee.com>
2021-11-18 21:28:51 +0100

    Test inline widgets

--- test/features/ui/render/widget_test.dart
+++ test/features/ui/render/widget_test.dart
@@ -5,23 +5,36 @@ import 'package:test/test.dart';
 
 import '../../../helpers/value_hook.dart';
 
+const _initialPrimaryState = '1';
+const _changedPrimaryState = '2';
+const _initialSecondaryState = '3';
+const _changedSecondaryState = '4';
+
 void main() {
-  StreamController<WidgetUpdateRequest> controller = StreamController();
-  State<String> state = State('hi');
-  _MockWidget widget = _MockWidget(state)
-    ..updateRequestReceiver = controller
-    ..line = 0;
+  late StreamController<WidgetUpdateRequest> controller;
+  late State<String> state;
+  late State<String> inlineState;
+  late _MockWidget widget;
+  late _MockInline inline;
 
   setUp(() {
     controller = StreamController();
-    state = State('hi');
+    state = State(_initialPrimaryState);
+    inlineState = State(_initialSecondaryState);
     widget = _MockWidget(state)
-      ..updateRequestReceiver = controller
+      ..updateRequestReceiver = controller.sink
       ..line = 0;
+
+    inline = _MockInline([
+      _MockWidget(inlineState),
+      _MockWidget(State('*')),
+    ])
+      ..updateRequestReceiver = controller.sink
+      ..line = 1;
   });
 
   test('Renders expected output', () {
-    expect(widget.render(), equals('mock-hi'));
+    expect(widget.render(), equals('mock-$_initialPrimaryState'));
   });
 
   test('Requests update on state change', () async {
@@ -32,10 +45,28 @@ void main() {
       hook.complete();
     });
 
-    state.value = 'bye';
+    state.value = _changedPrimaryState;
     await hook.awaitCompletion(Duration(seconds: 1));
-    expect(widget.content.value, equals('bye'));
-    expect(widget.render(), equals('mock-bye'));
+    expect(widget.content.value, equals(_changedPrimaryState));
+    expect(widget.render(), equals('mock-$_changedPrimaryState'));
+  });
+
+  test('Can render inline widgets', () {
+    expect(inline.render(), equals('mock-$_initialSecondaryState->mock-*'));
+  });
+
+  test('Children of inline widgets respond to state updates', () async {
+    final hook = VHook.empty();
+
+    inline.syncBody();
+    controller.stream.listen((event) {
+      expect(event.line, equals(1));
+      hook.complete();
+    });
+
+    inlineState.value = _changedSecondaryState;
+    await hook.awaitCompletion(Duration(seconds: 1));
+    expect(inline.render(), equals('mock-$_changedSecondaryState->mock-*'));
   });
 }
 
@@ -49,5 +80,14 @@ class _MockWidget extends Widget with StatefulWidget {
   @override
   String render() {
     return 'mock-${content.value}';
+  }
+}
+
+class _MockInline extends InlineWidget {
+  _MockInline(List<Widget> body) : super(body);
+
+  @override
+  String render() {
+    return body.map((e) => e.render()).join('->');
   }
 }
