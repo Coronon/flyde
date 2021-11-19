92482caba406a89c33ac72048ef306a5655ed928
Coronon <33808743+Coronon@users.noreply.github.com>
2021-11-19 16:00:21 +0100

    (Bin + Cli) catch erros properly

--- bin/flyde.dart
+++ bin/flyde.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:args/command_runner.dart';
 
 import 'package:flyde/features/cli/build_command.dart';
@@ -5,5 +7,9 @@ import 'package:flyde/features/cli/build_command.dart';
 void main(List<String> args) {
   final runner = CommandRunner('flyde', 'An easy to use C++ distributed compilation framework.')
     ..addCommand(BuildCommand())
-    ..run(args);
+    ..run(args).catchError((error) {
+      if (error is! UsageException) throw error;
+      print(error);
+      exit(64); // Exit code 64 indicates a usage error.
+    });
 }
