3b55e898ea1b326c3467b67c5138a1f2080e3e7f
Henrik <henrik@thoroee.com>
2021-11-20 23:25:12 +0100

    Test console tools

---
+++ test/core/console/bold_test.dart
@@ -0,0 +1,8 @@
+import 'package:flyde/core/console/bold.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('Applies ANSI code to print bold text to string', () {
+    expect('hello'.bold(), equals('\x1B[1mhello\x1B[0m'));
+  });
+}
---
+++ test/core/console/displayed_length_test.dart
@@ -0,0 +1,34 @@
+import 'package:flyde/core/console/bold.dart';
+import 'package:flyde/core/console/displayed_length.dart';
+import 'package:flyde/core/console/terminal_color.dart';
+import 'package:test/test.dart';
+
+void main() {
+  const testStrings = ['hello', 'world', '42'];
+
+  test('Can remove any ANSI codes from the string', () {
+    for (final testStr in testStrings) {
+      expect(
+        getDisplayedLength(testStr.bold()),
+        equals(testStr.length),
+      );
+
+      expect(
+        getDisplayedLength(TerminalColor.black.prepare(testStr)),
+        equals(testStr.length),
+      );
+    }
+
+    expect(
+      getDisplayedLength('\x1B[2K'),
+      equals(0),
+    );
+  });
+
+  test('Works with string extension', () {
+    expect(
+      '\x1B[2K'.displayedLength,
+      equals(0),
+    );
+  });
+}
