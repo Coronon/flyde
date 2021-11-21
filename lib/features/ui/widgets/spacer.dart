bafb39e73c5f9f836c8bdf6d1a7221d3d9d5f338
Henrik <henrik@thoroee.com>
2021-11-21 23:18:00 +0100

    Add dedicated spacer widget

---
+++ lib/features/ui/widgets/spacer.dart
@@ -0,0 +1,18 @@
+import 'dart:math';
+
+import 'package:flyde/features/ui/render/widget.dart';
+import 'package:flyde/features/ui/widgets/label.dart';
+
+/// Spacer element which resolves to a bunch of empty lines.
+class Spacer extends Widget with ContainerWidget {
+  /// Number of lines which should be empty.
+  final int _lines;
+
+  Spacer([this._lines = 1]);
+
+  @override
+  List<Widget> get children => List.filled(
+        max(_lines, 1),
+        Label.constant(''),
+      );
+}
