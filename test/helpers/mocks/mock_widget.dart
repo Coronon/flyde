3c54ad6fef4408fa87b9f9b23a9902c43c9ef6c4
Henrik <henrik@thoroee.com>
2021-12-08 14:13:04 +0100

    Add docs for mock widgets

--- test/helpers/mocks/mock_widget.dart
+++ test/helpers/mocks/mock_widget.dart
@@ -1,7 +1,17 @@
 import 'package:flyde/features/ui/render/widget.dart';
 
+/// Mock implementation of [Widget] using [StatefulWidget].
+///
+/// The mock exposes a stateful [content] property and subscribes to
+/// state changes.
+///
+/// The result of [render] is the state of content with the prefix `mock-`.
 class MockWidget extends Widget with StatefulWidget {
+  /// The stateful content of the widget.
   final State<String> content;
+
+  /// If the flag is true, do not add the `mock-` prefix to the
+  /// render output
   final bool straightForwardContent;
 
   MockWidget(this.content, {this.straightForwardContent = false}) {
@@ -18,6 +28,9 @@ class MockWidget extends Widget with StatefulWidget {
   }
 }
 
+/// Simple implementation of [InlineWidget].
+///
+/// The rendered output is the rendered body joined with `->`.
 class MockInline extends InlineWidget {
   MockInline(List<Widget> body) : super(body);
 
@@ -27,7 +40,12 @@ class MockInline extends InlineWidget {
   }
 }
 
+/// Simple mock for [ContainerWidget].
+///
+/// The children property returns an array containing two [MockWidget]s with
+/// the state of [content].
 class MockContainer extends Widget with ContainerWidget {
+  /// The state of the child widgets
   final State<String> content;
 
   MockContainer(this.content);
