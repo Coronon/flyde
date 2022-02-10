41960482b8de9698841931f899ad2de2cdc2ab95
Henrik <henrik@thoroee.com>
2022-02-10 16:56:27 +0100

    Wait for session to be ready

--- lib/features/cli/networking/create_client_session.dart
+++ lib/features/cli/networking/create_client_session.dart
@@ -4,9 +4,12 @@ import '../../../core/networking/websockets/session.dart';
 /// Creates a new session to the server using the [host] and [port].
 ///
 /// The created [ClientSession] is a wrapper around a `WebSocket` connection.
-ClientSession createClientSession(String host, int port) {
+Future<ClientSession> createClientSession(String host, int port) async {
   const String prefix = 'ws';
   final Uri uri = Uri.parse('$prefix://$host:$port');
+  final session = ClientSession(uri.toString())..middleware = const [protocolMiddleware];
 
-  return ClientSession(uri.toString())..middleware = const [protocolMiddleware];
+  await session.ready;
+
+  return session;
 }
